4f87e62c0e6ab4cf9178e8da92345b72d9b831e0
Rajan-sust <imrajon0@gmail.com>
2018-08-11 11:04:59 +0600

    Update genRealRand.cpp

--- genRealRand.cpp
+++ genRealRand.cpp
@@ -5,7 +5,7 @@ void genRealRand() {
   
   auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
   mt19937 generator(seed);
-  uniform_real_distribution<double> distribution(0.0,1.0); /*generate in close [a,b)*/
+  uniform_real_distribution<double> distribution(0.0,1.0); /*generate in close-open [a,b)*/
 
   for (int i = 1; i <= 10; i++) 
     cout << distribution(generator) << endl;
