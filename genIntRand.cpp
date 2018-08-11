f3379b932c3b03bf573de6991de57b3ab49e43d8
Rajan-sust <imrajon0@gmail.com>
2018-08-11 09:46:03 +0600

    Update genIntRand.cpp

--- genIntRand.cpp
+++ genIntRand.cpp
@@ -1,25 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include < bits / stdc++.h >
+  using namespace std;
 
+void genIntRand() {
+  auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  mt19937 generator(seed);
 
-void genIntRand()
-{
-	auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-	mt19937 generator(seed);
+  /*generate in close [a,b]*/
 
-	/*generate in close [a,b]*/
+  uniform_int_distribution < int > distribution(1, 10);
 
-    uniform_int_distribution<int> distribution(1,10);
+  for (int i = 1; i <= 10; i++) {
+    cout << distribution(generator) << endl;
+  }
 
-    for(int i=1;i<=10;i++){
-    	cout<<distribution(generator)<<endl;
-    }
-
-    
 }
 
-int main()
-{
-	genIntRand();
-	
+int main() {
+  genIntRand();
+
 }
