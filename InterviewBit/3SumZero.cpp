d7ccf98f5792d9d4ae893e66782b25e557767779
Rajan Saha Raju <imrajon0@gmail.com>
2018-08-31 14:50:04 +0600

    Create 3SumZero.cpp

---
+++ InterviewBit/3SumZero.cpp
@@ -0,0 +1,47 @@
+vector<vector<int> > Solution::threeSum(vector<int>& A)
+{
+
+    map<int, int> mp;
+
+    for (int x : A)
+        mp[x]++;
+
+    set<vector<int> > s;
+
+    set<int> unq(A.begin(), A.end());
+
+    if (mp.count(0)) {
+        if (mp[0] >= 3) {
+            s.insert(vector<int>{ 0, 0, 0 });
+        }
+    }
+
+    vector<int> num(unq.begin(), unq.end());
+
+    int len = num.size();
+
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            int x = num[i];
+            int y = num[j];
+            int z = -(x + y);
+            int c = 0;
+            if (z == x)
+                c++;
+            if (z == y)
+                c++;
+
+            auto it = mp.find(z);
+
+            if (it != mp.end()) {
+                if (it->second > c) {
+                    vector<int> a = { x, y, z };
+                    sort(a.begin(), a.end());
+                    s.insert(a);
+                }
+            }
+        }
+    }
+
+    return vector<vector<int> >(s.begin(), s.end());
+}
