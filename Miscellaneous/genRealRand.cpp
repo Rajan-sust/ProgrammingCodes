954f3b3050ca8149f1b975d416f94dd836fca877
Rajan Saha Raju <imrajon0@gmail.com>
2018-10-28 10:31:27 +0600

    Update genRealRand.cpp

--- Miscellaneous/genRealRand.cpp
+++ Miscellaneous/genRealRand.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
+/* gives real number in [0,1] */
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void genRealRand() {
-  
-  auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-  mt19937 generator(seed);
-  uniform_real_distribution<double> distribution(0.0,1.0); /*generate in close-open [a,b)*/
+void gen_realrand()
+{
 
-  for (int i = 1; i <= 10; i++) 
-    cout << distribution(generator) << endl;
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    mt19937 rng(seed);
+
+    for (int i = 1; i <= 10; i++)
+        cout << rng() / (1.0 * rng.max()) << endl;
 }
 
-int main() {
-  genRealRand();
-
+int main()
+{
+    gen_realrand();
 }
